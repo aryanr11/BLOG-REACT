1ff5447e50e7e0b1d2e3a7137c56cf3a2745aff1
Aryan rathod <aryancreative99@gmail.com>
2024-04-10 09:47:53 +0530

    .

--- src/Blog-API/Home.js
+++ src/Blog-API/Home.js
@@ -1,40 +1,39 @@
-import axios from 'axios';
-import React, { useEffect, useState, Component } from 'react'
-import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { Container, Row } from 'react-bootstrap';
+import axios from "axios";
+import React, { useEffect, useState, Component } from "react";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { Container, Row } from "react-bootstrap";
 
-import img from '../BLog Img/blog-1.webp'
-import main from '../BLog Img/main.webp'
+import img from "../BLog Img/blog-1.webp";
+import main from "../BLog Img/main.webp";
 
 import Slider from "react-slick";
 
 const Home = () => {
+  const [blog, setBlog] = useState([]);
 
-  const [blog, setBlog] = useState([])
-
-  const history = useHistory()
+  const history = useHistory();
 
   const getBlog = () => {
-    axios.get('https://blog-api-n83g.onrender.com/blog/find')
+    axios
+      .get("https://blog-node-x3z2.onrender.com/blog/find")
       .then((res) => {
         console.log(res.data.data);
-        setBlog(res.data.data)
+        setBlog(res.data.data);
       })
       .catch((error) => {
         alert(error.response.data.message);
-      })
-  }
+      });
+  };
 
   useEffect(() => {
-    getBlog()
-  }, [])
+    getBlog();
+  }, []);
 
   const card = (id) => {
     console.log(id);
-    localStorage.setItem('blogid', id)
-    history.push('/blog')
-  }
-
+    localStorage.setItem("blogid", id);
+    history.push("/blog");
+  };
 
   // Slider Setting
   const settings = {
@@ -50,7 +49,6 @@ const Home = () => {
 
   return (
     <div>
-
       {/* Main IMG */}
       <div className="container-fluid  position-relative  p-0">
         <div className="row align-items-center">
@@ -63,7 +61,9 @@ const Home = () => {
       <div className="container-fluid pos-ab">
         <div className="row align-items-center">
           <div className="p-0">
-            <h1 className='fs-60 fs-sm-6'>DESIGN <span className='fs-2 fs-sm-6 for'>FOR</span> LIFE</h1>
+            <h1 className="fs-60 fs-sm-6">
+              DESIGN <span className="fs-2 fs-sm-6 for">FOR</span> LIFE
+            </h1>
           </div>
         </div>
       </div>
@@ -76,7 +76,7 @@ const Home = () => {
               blog.map((el, index) => {
                 return <div className="col-lg-4 position-relative img-hov" key={index}>
                   <div className=''>
-                    <img width="100%" className='' onClick={() => card(el._id)} src={'https://blog-api-n83g.onrender.com/images/' + el.image} alt="" />
+                    <img width="100%" className='' onClick={() => card(el._id)} src={'https://blog-node-x3z2.onrender.com/images/' + el.image} alt="" />
                   </div>
                   <div className='pos-ab-1 text-center '>
                     <h3 className='mt-2 '>{el.title}</h3>
@@ -90,40 +90,53 @@ const Home = () => {
         </Container>
       </Container> */}
 
-
       {/* Slider */}
-      <Container fluid className='mt-5 mb-5'>
-        <Row className='d-flex'>
+      <Container fluid className="mt-5 mb-5">
+        <Row className="d-flex">
           <div className="slider-container p-0">
             <Slider {...settings}>
-              {
-                blog.map((el, index) => {
-                  return <div className="col-lg-4 m-2 position-relative img-hov" key={index}>
-                    <div className='m-2'>
-                      <div className=''>
-                        <img width="100%" className='' onClick={() => card(el._id)} src={'https://blog-api-n83g.onrender.com/images/' + el.image} alt="" />
+              {blog.map((el, index) => {
+                return (
+                  <div
+                    className="col-lg-4 m-2 position-relative img-hov"
+                    key={index}
+                  >
+                    <div className="m-2">
+                      <div className="">
+                        <img
+                          width="100%"
+                          className=""
+                          onClick={() => card(el._id)}
+                          src={
+                            "https://blog-node-x3z2.onrender.com/images/" +
+                            el.image
+                          }
+                          alt=""
+                        />
                       </div>
-                      <div className='pos-ab-1 text-center '>
-                        <h3 className='mt-2 '>{el.title}</h3>
+                      <div className="pos-ab-1 text-center ">
+                        <h3 className="mt-2 ">{el.title}</h3>
                         <hr />
-                        <p className='ms-1'><a href="/blog" onClick={() => card(el._id)} className='text-white fs-5 Read-more mt-3'>Read More..!</a></p>
+                        <p className="ms-1">
+                          <a
+                            href="/blog"
+                            onClick={() => card(el._id)}
+                            className="text-white fs-5 Read-more mt-3"
+                          >
+                            Read More..!
+                          </a>
+                        </p>
                       </div>
                     </div>
                   </div>
-                })
-              }
+                );
+              })}
             </Slider>
           </div>
         </Row>
       </Container>
-
-
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Home
-
-
+export default Home;
